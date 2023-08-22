7803fc1e4e11fd45b932fb8ca4a998d4048c25a0
Ryan McClue <re.mcclue@protonmail.com>
2023-08-22 14:12:33 +1000

    Arena

--- ui.cpp
+++ ui.cpp
@@ -7,6 +7,19 @@
 int
 main(int argc, char *argv[])
 {
+  MEM_ARENA_TEMP_BLOCK(some_arena, scratch_arena)
+  {
+
+  }
   printf("%ld\n", sizeof(memory_index));
   return 0;
 }
+
+// as recieves arena, will be allocating permanent results to caller
+func(MemArena *arena) -> Node *
+// so, if doing temp work, won't pass in, rather use global temp?
+
+// linear allocator
+// dynamic with linked list of arenas?
+
+// IMPORTANT(Ryan): If need variable lifetimes, can build atop of arena with a free-list/pool allocator
