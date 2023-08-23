97815beca0598fb4125fb2afe8cda24fd1cd75af
Ryan McClue <re.mcclue@protonmail.com>
2023-08-23 11:16:23 +1000

    Sound playing

--- ui.cpp
+++ ui.cpp
@@ -4,8 +4,12 @@
 
 #include "raylib.h"
 
-// TODO(Ryan): ranger searching and file previewing
+// TODO(Ryan): ranger previewing (probably have to reinstall python to get ueberzug!)
 // TODO(Ryan): git blame and git content inside vim
+// git blame -w -M3 -- file | grep line-num) --> obtain commit-hash
+// !hash=$(git --no-pager blame -w -M3 -L 17,17 -- ui.cpp | awk '{print $1}'); git difftool "$hash"^.."$hash" -- ui.cpp
+// git difftool 0173b443^..0173b443 -- <file>
+// git show commit-hash
 
 int
 main(int argc, char *argv[])
@@ -13,7 +17,10 @@ main(int argc, char *argv[])
   global_debugger_present = linux_was_launched_by_gdb();
 
   InitAudioDevice();
-  Sound sound = LoadSound();
+  Sound sound = LoadSound("the-tower-of-dreams.ogg");
+  PlaySound(sound);
+
+  sleep(1);
 
 #if 0
   MEM_ARENA_TEMP_BLOCK(some_arena, scratch_arena)
