04d8fa7957c007c4f0e454c477d809fe5c9fef40
Ryan McClue <re.mcclue@protonmail.com>
2023-11-23 10:54:07 +1100

    j

--- misc/metrics.sql
+++ misc/metrics.sql
@@ -6,8 +6,17 @@ create table if not exists build_machines (
   linker text not null
 );
 
-create table if not exists top10_symbols (
-  hash text primary key,
+create table if not exists build_metrics (
+  created_at timestamp not null default CURRENT_TIMESTAMP,
+  name text not null,
+  hash text not null,
+  parent_hash text not null,
+  build_type text not null,
+  text_size integer not null,
+  data_size integer not null,
+  bss_size integer not null,
+  build_time real not null,
+  loc integer not null,
   sym0_name text not null,
   sym0_val integer not null,
   sym1_name text not null,
@@ -27,22 +36,8 @@ create table if not exists top10_symbols (
   sym8_name text not null,
   sym8_val integer not null,
   sym9_name text not null,
-  sym9_val integer not null
-);
-
-create table if not exists build_metrics (
-  created_at timestamp not null default CURRENT_TIMESTAMP,
-  name text not null,
-  hash text not null,
-  parent_hash text not null,
-  build_type text not null,
-  text_size integer not null,
-  data_size integer not null,
-  bss_size integer not null,
-  build_time real not null,
-  loc integer not null,
-  build_machine integer references build_machines(id),
-  top10_symbol text references top10_symbols(id),
+  sym9_val integer not null,
+  build_machine text references build_machines(text),
   constraint hash_different check (parent_hash != hash),
   primary key (hash, build_type)
 );
